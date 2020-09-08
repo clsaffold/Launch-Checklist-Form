e43a1711817ea63fb0c563678527d8190ccef80e
csaffold <ceciliasaffold@gmail.com>
2020-09-08 15:45:51 -0500

    Various Changes

--- script.js
+++ script.js
@@ -13,8 +13,9 @@
 */
 window.addEventListener("load", function() {
    fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
-      response.json().then(function(json) {
+      response.json().then(function(json){
          console.log(json);
+         return
       });   
   });
    let form = document.querySelector("form");
